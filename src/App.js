19249b9085fbf9810e6581abde6b6e5aadb8073f
Marcus <107251339+marcusprodev@users.noreply.github.com>
2022-12-16 04:09:01 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Imagem from '../src/img/santosfc.jpg'
+
+function App() {
+  return (
+    <div>
+      <article>
+        <img src={Imagem}/>
+        <h2>2</h2>
+        <h3>3</h3>
+      </article>
+    </div>
+  );
+}
+
+export default App;
